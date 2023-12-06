190d13904b3262b682c1188f9e2873118639b6a7
Anany Sachan <72132519+wizanyx@users.noreply.github.com>
2023-12-06 17:21:26 -0500

    Added Story

--- story.py
+++ story.py
@@ -1,4 +1,4 @@
-story = [
+STORY = [[
     "You are a normal Human.",
     "And it was a normal day.",
     "And then you saw it.",
@@ -9,4 +9,35 @@ story = [
     "And stood up to fight it",
     "To fight that beast... that dog.",
     "And everything changed forever..."
+], [
+    "You've done it...",
+    "You've defeated your fated enemy.",
+    "You killed the dog...",
+    "'I'm sure it was evil...'",
+    "'It has to be...'",
+    "'But it's last whimper was so sad.'",
+    "'NO!!! I won't fall for these petty tricks'",
+    "'Trying to waver my resolve even at its last breath'",
+    "'As expected on evil creature'",
+    "You firm your gaze and step forward",
+    "And then you hear it...",
+    "That heavenly sound...",
+    "As if to answer your resolve",
+    '"ITZA MEE DARRIOOO"'
 ]
+]
+
+
+class Story:
+    def __init__(self):
+        self.stage = 0
+
+    def next_line(self):
+        if STORY[self.stage]:
+            return STORY[self.stage].pop(0)
+
+    def characters(self):
+        if self.stage == 0:
+            return [["Man"], ["Dog"]]
+        elif self.stage == 1:
+            return [["Man", "Dario"], ["Angel Dog", "Monald Muck"]]
