5bb13c46c113430ed234a7fec7412c1e5da8e820
Zuritee <candylichqwq@gmail.com>
2023-12-06 21:20:46 -0500

    Added character data

--- data.py
+++ data.py
@@ -42,6 +42,61 @@ characters = [
         ],
         "camp": 0
     }, {
+        "name": "Jesus",
+        "hp": 150,
+        "moves": [
+            {
+                "name": "Jesus Kick",
+                "dmg": 15,
+                "speed": 2
+            }, {
+                "name": "Jesus Lightning",
+                "dmg": 20,
+                "speed": 4,
+                "percent": True
+                "status": {
+                    "def_boost": 15,
+                    "duration": 4
+                }
+            }, {
+                "name": "Jesus Beam",
+                "dmg": 30,
+                "speed": 3,
+                "percent": False
+            }
+        ],
+        "camp": 0
+    }, {
+        "name": "Wojak",
+        "hp": 100,
+        "moves": [
+            {
+                "name": "Mald",
+                "dmg": 25,
+                "speed": 2
+            }, {
+                "name": "Cope",
+                "dmg": 0,
+                "speed": 4,
+                "status": {
+                    "def_boost": 30,
+                    "duration": 2
+                }
+            }
+        ],
+        "camp": 0
+    }, {
+        "name": "Saitama",
+        "hp": 250,
+        "moves": [
+            {
+                "name": "Normal Punch",
+                "dmg": 2000,
+                "speed": 2
+            }
+        ],
+        "camp": 0
+    }, { # Bot Characters
         "name": "Dog",
         "hp": 80,
         "moves": [
@@ -90,6 +145,175 @@ characters = [
                     "def_boost": 40,
                     "duration": 2
                 }
+            }
+        ],
+        "camp": 1
+    }, {
+        "name": "Devil Dog",
+        "hp": 120,
+        "moves": [
+            {
+                "name": "Bite",
+                "dmg": 30,
+                "speed": 3
+            }, {
+                "name": "Bark",
+                "dmg": 1,
+                "speed": 5,
+                "percent": True,
+                "status": {
+                    "dmg_boost": 30,
+                    "def_boost": 10,
+                    "duration": 2
+                }
+            }, {
+                "name": "Unholy Crap",
+                "dmg": 10,
+                "speed": 3,
+                "percent": True,
+                "status": {
+                    "dmg_boost": 40,
+                    "def_boost": 10,
+                    "duration": 2
+                }
+            }
+        ],
+        "camp": 1
+    }, {
+        "name": "Elder Dog",
+        "hp": 140,
+        "moves": [
+            {
+                "name": "Bite",
+                "dmg": 30,
+                "speed": 3
+            }, {
+                "name": "Bark",
+                "dmg": 1,
+                "speed": 5,
+                "percent": True,
+                "status": {
+                    "dmg_boost": 30,
+                    "def_boost": 10,
+                    "duration": 2
+                }
+            }, {
+                "name": "Unknowable Crap",
+                "dmg": 10,
+                "speed": 3,
+                "percent": True,
+                "status": {
+                    "dmg_boost": 40,
+                    "def_boost": 40,
+                    "duration": 2
+                }
+            }
+        ],
+        "camp": 1
+    }, {
+        "name": "Monal Muck",
+        "hp": 130,
+        "moves": [
+            {
+                "name": "Shlong Swipe",
+                "dmg": 35,
+                "speed": 3
+            }, {
+                "name": "Quack",
+                "dmg": 1,
+                "speed": 5,
+                "percent": True,
+                "status": {
+                    "dmg_boost": 30,
+                    "def_boost": 10,
+                    "duration": 3
+                }
+            }, {
+                "name": "Bread Toss",
+                "dmg": 20,
+                "speed": 3,
+                "percent": True,
+                "status": {
+                    "def_boost": 10,
+                    "duration": 2
+                }
+            }, {
+                "name": "Where is Mom",
+                "dmg": 0,
+                "speed": 3,
+                "status": {
+                    "def_boost": 40,
+                    "duration": 2
+                }
+            }
+        ],
+        "camp": 1
+    }, {
+        "name": "Jreddy Jazzbear",
+        "hp": 150,
+        "moves": [
+            {
+                "name": "Bite of 87",
+                "dmg": 40,
+                "speed": 3
+            }, {
+                "name": "Kill Children",
+                "dmg": 10,
+                "speed": 5,
+                "percent": True,
+                "status": {
+                    "dmg_boost": 40
+                    "duration": 1
+                }
+            }, {
+                "name": "Ghostly Jazz",
+                "dmg": 0,
+                "speed": 3,
+                "percent": True,
+                "status": {
+                    "def_boost": 50,
+                    "duration": 2
+                }
+            }
+        ],
+        "camp": 1
+    }, {
+        "name": "Garfield",
+        "hp": 180,
+        "moves": [
+            {
+                "name": "Cat Attack",
+                "dmg": 35,
+                "speed": 3
+            }, {
+                "name": "Monday Power",
+                "dmg": 15,
+                "speed": 5,
+                "percent": True,
+                "status": {
+                    "dmg_boost": 50,
+                    "duration": 2
+                }
+            }, {
+                "name": "Good Meal",
+                "dmg": 0,
+                "speed": 3,
+                "percent": True,
+                "status": {
+                    "def_boost": 50,
+                    "duration": 2
+                }
+            }
+        ],
+        "camp": 1
+    }, {
+        "name": "Professor Guarnera",
+        "hp": 1000,
+        "moves": [
+            {
+                "name": "Fail",
+                "dmg": 2000,
+                "speed": 3
             }
         ],
         "camp": 1
